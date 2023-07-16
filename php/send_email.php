ed0f4847dc0fab7ad50b733d3c8f3849c4d36ed5
lucas-alves1 <du.dudulucas@gmail.com>
2023-07-15 21:12:02 -0300

    adição página links úteis

--- php/send_email.php
+++ php/send_email.php
@@ -1,26 +1,21 @@
 <?php
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // $to = 'contato@marianobaldiniadv.com.br';
     $to = 'lucas.lago.cont@gmail.com';
     $email = $_POST['email'];
     $assunto = $_POST['assunto'];
     $mensagem = $_POST['mensagem'];
     $subject = 'Novo email encaminhado pelo site';
   
-    // Construct the email headers
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=utf-8\r\n";
   
-    // Construct the email body
     $message = "<html><body>";
     $message .= "<h2>$assunto</h2>";
     $message .= "<p>$mensagem</p>";
     $message .= "</body></html>";
   
-    // Send the email
     $success = mail($to, $subject, $message, $headers);
   
     if ($success) {
@@ -29,5 +24,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       echo "Erro";
     }
 }
+
+header("Location: index.html");
+exit();
 
 ?>
